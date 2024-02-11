f0556dd1c9eabef51eebdb9b52a78b29792e4f2c
laurisseau Joseph <reso0208@gmail.com>
2024-02-11 03:37:27 -0500

    commit

--- src/main/java/com/etta/edtech/EdtechApplication.java
+++ src/main/java/com/etta/edtech/EdtechApplication.java
@@ -3,6 +3,8 @@ package com.etta.edtech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class EdtechApplication {
 
@@ -12,8 +14,16 @@ public class EdtechApplication {
 	}
 
 	public static void test(){
-		System.out.println(System.getenv("spring_datasource_url"));
-		System.out.println(System.getenv("spring_datasource_username"));
-		System.out.println(System.getenv("spring_datasource_password"));
+		// Get all environment variables
+		Map<String, String> envVariables = System.getenv();
+
+		// Iterate over the map and print each variable and its value
+		for (Map.Entry<String, String> entry : envVariables.entrySet()) {
+			String variable = entry.getKey();
+			String value = entry.getValue();
+			System.out.println(variable + " =loop= " + value);
+		}
+
+		System.out.println(System.getenv("DB_URL") + " last env try");
 	}
 }
