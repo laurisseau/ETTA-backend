1c3c5e64e2819ea58a5326edcf7b88e10ea33e5f
laurisseau Joseph <reso0208@gmail.com>
2024-02-11 02:57:01 -0500

    commit

--- src/test/java/com/etta/edtech/EdtechApplicationTests.java
+++ src/test/java/com/etta/edtech/EdtechApplicationTests.java
@@ -3,10 +3,10 @@ package com.etta.edtech;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class EdtechApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
 	}
 
