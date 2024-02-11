86487ece05a6b43735f05e0d9fd89617c7cbc450
laurisseau Joseph <reso0208@gmail.com>
2024-02-11 03:14:01 -0500

    commit

--- src/main/java/com/etta/edtech/EdtechApplication.java
+++ src/main/java/com/etta/edtech/EdtechApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EdtechApplication {
 
 	public static void main(String[] args) {
+		test();
 		SpringApplication.run(EdtechApplication.class, args);
 	}
 
+	public static void test(){
+		System.out.println(System.getenv("spring_datasource_url"));
+		System.out.println(System.getenv("spring_datasource_username"));
+		System.out.println(System.getenv("spring_datasource_password"));
+	}
 }
