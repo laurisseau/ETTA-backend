9b8d74cbf995da17137f5ed2f28840022cbab647
laurisseau Joseph <reso0208@gmail.com>
2024-02-13 00:28:20 -0500

    commit

--- src/main/java/com/etta/edtech/EdtechApplication.java
+++ src/main/java/com/etta/edtech/EdtechApplication.java
@@ -9,21 +9,7 @@ import java.util.Map;
 public class EdtechApplication {
 
 	public static void main(String[] args) {
-		test();
 		SpringApplication.run(EdtechApplication.class, args);
 	}
 
-	public static void test(){
-		// Get all environment variables
-		Map<String, String> envVariables = System.getenv();
-
-		// Iterate over the map and print each variable and its value
-		for (Map.Entry<String, String> entry : envVariables.entrySet()) {
-			String variable = entry.getKey();
-			String value = entry.getValue();
-			System.out.println(variable + " =loop= " + value);
-		}
-
-		System.out.println(System.getenv("DB_URL") + " last env try");
-	}
 }
